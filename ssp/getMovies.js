f79865955ee8c19dfac4c9eeea2bcd51c102e023
@EnaidaH <enaidahuskic@yahoo.com>
2022-11-19 10:50:18 +0100

    updated

--- ssp/getMovies.js
+++ ssp/getMovies.js
@@ -1,7 +1,7 @@
-// import { server } from '../config'
+import { server } from '../config'
 
 export const getMovies = async () => {
-  const response = await fetch(`http://localhost:3000/api/`)
+  const response = await fetch(`${server}/api/`)
   if (response.ok) {
     return response.json()
   } else {
