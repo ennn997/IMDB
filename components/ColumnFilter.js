7d1a3c232d7c248999905e6becaf795aa918f0cc
@EnaidaH <enaidahuskic@yahoo.com>
2022-11-20 22:44:22 +0100

    updated

--- components/ColumnFilter.js
+++ components/ColumnFilter.js
@@ -39,7 +39,7 @@ const ColumnFilter = ({ column, table }) => {
         value={columnFilterValue || ''}
       >
         <option value="">-</option>
-        <option value="0">0</option>
+        <option value="0">0-1</option>
         <option value="1">1-2</option>
         <option value="2">2-3</option>
         <option value="3">3-4</option>
