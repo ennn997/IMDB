900bd5f97e017e8962a09842b407bd5e900d3d33
@EnaidaH <enaidahuskic@yahoo.com>
2022-11-20 22:41:11 +0100

    updated

--- components/ColumnFilter.js
+++ components/ColumnFilter.js
@@ -38,39 +38,18 @@ const ColumnFilter = ({ column, table }) => {
         onChange={(e) => column.setFilterValue(e.target.value || undefined)}
         value={columnFilterValue || ''}
       >
-        <option value="" align="center">
-          -
-        </option>
-        <option value="0" align="center">
-          0-1
-        </option>
-        <option value="1" align="center">
-          1-2
-        </option>
-        <option value="2" align="center">
-          2-3
-        </option>
-        <option value="3" align="center">
-          3-4
-        </option>
-        <option value="4" align="center">
-          4-5
-        </option>
-        <option value="5" align="center">
-          5-6
-        </option>
-        <option value="6" align="center">
-          6-7
-        </option>
-        <option value="7" align="center">
-          7-8
-        </option>
-        <option value="8" align="center">
-          8-9
-        </option>
-        <option value="9" align="center">
-          9-10
-        </option>
+        <option value="">-</option>
+        <option value="0">0</option>
+        <option value="1">1-2</option>
+        <option value="2">2-3</option>
+        <option value="3">3-4</option>
+        <option value="4">4-5</option>
+        <option value="5">5-6</option>
+        <option value="6">6-7</option>
+        <option value="7">7-8</option>
+        <option value="8">8-9</option>
+        <option value="9">9-10</option>
+        <option value="10">10.0</option>
       </Select>
     </>
   )
--- components/Columns.js
+++ components/Columns.js
@@ -10,31 +10,26 @@ const roundedToFixed = (input, digits) => {
 export const COLUMNS = [
   columnHelper.accessor('title', {
     header: () => 'Movie name',
-    id: 'title',
     enableColumnFilter: false,
     size: 200,
   }),
   columnHelper.accessor('ratings', {
     header: () => 'Ratings',
-    id: 'ratings',
     cell: (num) => roundedToFixed(num.getValue(), 1),
     enableGlobalColumnFilter: false,
     filterFn: (row, id, filterValue) => row.getValue(id).startsWith(filterValue),
   }),
   columnHelper.accessor('year', {
     header: () => 'Year',
-    id: 'year',
     enableGlobalColumnFilter: false,
   }),
   columnHelper.accessor('director', {
     header: () => 'Director',
-    id: 'director',
     enableColumnFilter: false,
     size: 180,
   }),
   columnHelper.accessor('starring', {
     header: () => 'Starring',
-    id: 'staring',
     enableColumnFilter: false,
     size: 150,
   }),
