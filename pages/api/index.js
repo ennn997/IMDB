df27f60f07cf00f8d5143bffd9a7b1c52b65b3ee
@EnaidaH <enaidahuskic@yahoo.com>
2022-12-06 13:15:17 +0100

    add Joi validation schema to validate request body

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,5 +1,15 @@
 import prisma from '../../lib/prisma'
 
+const Joi = require('joi')
+
+const movieSchema = Joi.object().keys({
+  title: Joi.string().required(),
+  ratings: Joi.number().min(0).max(10).precision(1).required(),
+  year: Joi.number().integer().positive().max(2022).required(),
+  director: Joi.string().required(),
+  starring: Joi.string().required(),
+})
+
 export default async function handler(req, res) {
   if (req.method === 'GET') {
     try {
@@ -9,12 +19,22 @@ export default async function handler(req, res) {
       res.status(500).json({ error: 'Oops, something went wrong! ' })
     }
   } else if (req.method === 'POST') {
-    const { title, ratings, year, director, starring } = req.body
+    const { error, value } = movieSchema.validate(req.body)
     try {
-      const movie = await prisma.movie.create({
-        data: { title, ratings, year, director, starring },
-      })
-      res.status(200).json(movie)
+      if (error) {
+        res.status(400).json({ error: 'All fields must be filled out' })
+      } else {
+        const movie = await prisma.movie.create({
+          data: {
+            title: value.title,
+            ratings: value.ratings,
+            year: value.year,
+            director: value.director,
+            starring: value.starring,
+          },
+        })
+        res.status(200).json(movie)
+      }
     } catch (error) {
       res.status(500).json({ error: 'Error' })
     }
