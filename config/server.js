4276fb533a7eb4a60852b7cdf9d8cbefdcc8f59b
@EnaidaH <enaidahuskic@yahoo.com>
2023-01-13 14:19:12 +0100

    upgrade next js and add domain url

--- config/server.js
+++ config/server.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production'
 
-export const server = dev ? 'http://localhost:3000' : 'https://imdb-7g6ik2j0y-ennn997.vercel.app'
+export const server = dev ? 'http://localhost:3000' : 'https://imdb-six.vercel.app/'
