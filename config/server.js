f909c58072508bf7f67cd10f1a68b1df91ff06cd
@EnaidaH <enaidahuskic@yahoo.com>
2023-01-15 20:18:20 +0100

    change domain url

--- config/server.js
+++ config/server.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production'
 
-export const server = dev ? 'http://localhost:3000' : 'https://imdb-six.vercel.app'
+export const server = dev ? 'http://localhost:3000' : 'https://imdb-git-dev-ennn997.vercel.app/'
